6b80872af87cc2e179c9c4e9a922a0c58f4fffee
Vincent Jousse <vincent.jousse@devorigin.fr>
2010-04-19 22:32:01 +0200

    Include of Base classes are now relative

--- modules/sfSimpleForum/actions/actions.class.php
+++ modules/sfSimpleForum/actions/actions.class.php
@@ -9,7 +9,7 @@
  */
 
 // autoloading for plugin lib actions is broken as at symfony-1.0.2
-require_once(sfConfig::get('sf_plugins_dir'). '/sfDoctrineSimpleForumPlugin/modules/sfSimpleForum/lib/BasesfSimpleForumActions.class.php');
+require_once(dirname(__FILE__). '/../lib/BasesfSimpleForumActions.class.php');
 
 class sfSimpleForumActions extends BasesfSimpleForumActions
 {
--- modules/sfSimpleForum/actions/components.class.php
+++ modules/sfSimpleForum/actions/components.class.php
@@ -7,7 +7,7 @@
  * @version    SVN: $Id$
  */
 
-require_once(sfConfig::get('sf_plugins_dir'). '/sfSimpleForumPlugin/modules/sfSimpleForum/lib/BasesfSimpleForumComponents.class.php');
+require_once(dirname(__FILE__) . '/../lib/BasesfSimpleForumComponents.class.php');
 
 class sfSimpleForumComponents extends BasesfSimpleForumComponents
 {
